be9e96190c87307a33800c775f1e0dd53782439b
John Figueiredo <john@johnfigueiredo.com>
2016-01-30 12:39:23 -0600

    Add closure examples

--- closure.js
+++ closure.js
@@ -2,6 +2,8 @@
 // its lexical scope even when the function is executing OUTSIDE
 // of its lexical scope
 
+// Example of basic closure
+
 function foo() {
   var a = 2;
 
@@ -14,4 +16,16 @@ function foo() {
 
 baz = foo();
 baz();
+
+
+// Asynchronous timeouts using closures
+
+for (var i=1; i<=5; i++) {
+  (function() {
+    var j = i;
+    setTimeout( function timer(){
+      console.log(j);
+    }, j*1000);
+  })();
+}
 
